19bf949e4f153461dc9cb53580ac22b1ace540b3
arzman <arz.man@free.fr>
2017-02-27 22:03:06 +0100

    ajout de base

---
+++ Compta-Base/src/compta/base/Perspective.java
@@ -0,0 +1,18 @@
+package compta.base;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		
+		
+		layout.setEditorAreaVisible(false);
+		
+		
+	}
+	
+	
+	
+}
